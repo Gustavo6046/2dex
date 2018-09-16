9de6eb793ec7e34481fe7a811174eaee147304d1
Gustavo R. 'Gustavo6046' Rehermann <gugurehermann@gmail.com>
2018-09-16 02:31:09 -0300

    Java documentation for GitHub Pages.

--- src/usr/gustavo6046/lib/j2dex/display/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package holds all the display logic that 2DEX contains. This is a
- * smaller abstraction, though, that is mostly used to allow for easier game
- * event development using handlers.
- * 
- * @author Gustavo6046
- */
-package usr.gustavo6046.lib.j2dex.display;
--- src/usr/gustavo6046/lib/j2dex/input/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package is a higher level abstraction on Java2D's input listening
- * methods. This package was adapted specifically for use along with 2DEX.
- * 
- * @author Gustavo6046
- */
-package usr.gustavo6046.lib.j2dex.input;
